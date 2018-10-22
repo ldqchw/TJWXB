5f4a22bb3d8e842c617f284a339c1eee71aac390
ldq <961811769@qq.com>
2018-10-22 14:35:12 +0800

    add Template

--- README.md
+++ README.md
@@ -8,6 +8,14 @@
 <h2>使用方法</h2>
 <p>python Dwszb_Check.py
 <p>安装所需要的库PS:Windows运行pyttsx需要安装pywin32
-
+<h1>2、Template</h1>
+<ul>
+<li>1.1、使用figlet生成banner
+<li>1.2、使用getopt命令行输入
+<li>1.3、使用xlwings读写Excel
+<li>1.4、使用win32com进行Word批量替换
+<h2>使用方法</h2>
+<p>python XQZG-Template.py -h 
+<p>安装所需要的库
 
 
--- Template/YHGZ/YHGZ-Template.py
+++ Template/YHGZ/YHGZ-Template.py
@@ -5,7 +5,6 @@ import getopt
 import win32com
 import xlwings as xw
 from win32com.client import Dispatch, constants
-#from envelopes import Envelope
 
 def banner():
     print r'''
@@ -22,12 +21,15 @@ py price.py [option][value]...
 
 -h or --help     --help for detail
 -o or --ofile    输出生成文件路径，默认为当前目录
--i or --ifile    输入配置文件路径，默认是当前目录:XQZG-Template-Parameters.xlsx
--t or --template 输入模板文件路径，默认为当前目录:XQZG-Template-V8.docx
+-i or --ifile    输入配置文件路径，默认是当前目录:YHGZ-Template-Parameters.xlsx
+-t or --template 输入模板文件路径，默认为当前目录:YHGZ-Template-V9.docx
 '''
+def pwd():
+    pwd = os.path.split(os.path.realpath(__file__))[0]
+    return pwd;
 def get(argv):
-   inputfile = pwd() + u'\\XQZG-Template-Parameters.xlsx'
-   inputfile_T = pwd() + u"\\XQZG-Template-V8.docx"
+   inputfile = pwd() + u'\\YHGZ-Template-Parameters.xlsx'
+   inputfile_T = pwd() + u"\\YHGZ-Template-V9.docx"
    outputpath = pwd()
    try:
       opts, args = getopt.getopt(argv,"hi:t:o:",["help","ifile=","template=","ofile="])
@@ -48,7 +50,7 @@ def get(argv):
       elif opt in ("-i", "--ifile"):
          inputfile = arg
       elif opt in ("-t","--template"):
-         inputfile_T = arg
+         inputfile_T = pwd() + arg
       elif opt in ("-o", "--ofile"):
          outputpath = arg
          try:
@@ -60,16 +62,20 @@ def get(argv):
              sys.exit(1)
    banner()
    return inputfile,inputfile_T,outputpath
-
-def pwd():
-    pwd = os.path.split(os.path.realpath(__file__))[0]
-    return pwd;
-
+def check_outputpath(outputpath):
+    try:
+        print u"检查输出路径是否有写权限..."
+        open(outputpath + "/rtest", 'w')
+    except Exception, e:
+        print u"输出文件不能写入，请用有写权限的运行..."
+        sys.exit(1)
+    var = u"检查输出路径成功..."
+    return var
 def conf(pathxlsx):
     app_xlsx = xw.App(visible=True, add_book=False)
     wb = app_xlsx.books.open(pathxlsx)
     print u'正在从%s文件中获取配置...' % pathxlsx
-    lists = wb.sheets[0].range('B1:B7').value
+    lists = wb.sheets[0].range('B1:B9').value
     wb.close()
     app_xlsx.quit()
     for x in lists:
@@ -92,7 +98,10 @@ def replace(pathword,outputpath):
     NewData2 = lists[5]
     OldData3 = u'2018-xx-xx xx:xx:xx'
     NewData3 = lists[6]
-
+    OldIp1 = u'XXX.XXX.XXX'
+    NewIp1 = lists[7]
+    OldIp2 = u'XX.XX.XX.XX'
+    NewIp2 = lists[8]
     app = win32com.client.Dispatch('Word.Application')
     doc = app.Documents.Open(pathword)
     print u'正在生成模板中...'
@@ -107,27 +116,14 @@ def replace(pathword,outputpath):
     app.Selection.Find.Execute(OldData1, False, False, False, False, False, True, 1, False, NewData1, 2)
     app.Selection.Find.Execute(OldData2, False, False, False, False, False, True, 1, False, NewData2, 2)
     app.Selection.Find.Execute(OldData3, False, False, False, False, False, True, 1, False, NewData3, 2)
+    app.Selection.Find.Execute(OldIp1, False, False, False, False, False, True, 1, False, NewIp1, 2)
+    app.Selection.Find.Execute(OldIp2, False, False, False, False, False, True, 1, False, NewIp2, 2)
     print u"正在保存模板中..."
     print outputpath + "\\" + NewCompany + '.docx'
     doc.SaveAs(outputpath + "\\" + NewCompany + '.docx')
     doc.Close()
-    # app.Documents.Close()
     app.Quit()
     print u'文件生成成功...'
-def check_outputpath(outputpath):
-    try:
-        print u"检查输出路径是否有写权限..."
-        open(outputpath + "/rtest", 'w')
-    except Exception, e:
-        # ul_log.write(ul_log.fatal,'%s,%s,@line=%d,@file=%s' \
-        # %(type(e),str(e),sys._getframe().f_lineno,sys._getframe().f_code.co_filename))
-        print u"输出文件不能写入，请用有写权限的运行..."
-        sys.exit(1)
-    var = u"检查输出路径成功..."
-    return var
-
-
-
 if __name__ == "__main__":
     inputfile,inputfile_T, outputpath = get(sys.argv[1:])
     print u"获取当前运行路径为：", pwd()
@@ -138,8 +134,3 @@ if __name__ == "__main__":
     pathxlsx = inputfile
     pathword = inputfile_T
     replace(pathword,outputpath)
-
-
-
-
-
